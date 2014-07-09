17d4cc2e426f268a5e49b1ca06faad3942bb62ad
Moritz Schlarb <moschlar@metalabs.de>
2014-07-09 23:10:48 +0200

    Additional example with Sigma

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -7,9 +7,15 @@ transparentcrypto.preview = function() {
 		"transparentcrypto-window-preview",
 		"chrome,width=400,height=300");
 };
-transparentcrypto.graph = function() {
+transparentcrypto.graph_d3 = function() {
 	window.open(
-		"chrome://transparentcrypto/content/graph.xul",
-		"transparentcrypto-window-graph",
+		"chrome://transparentcrypto/content/graph_d3.xul",
+		"transparentcrypto-window-graph-d3",
+		"chrome,width=600,height=400");
+};
+transparentcrypto.graph_sigma = function() {
+	window.open(
+		"chrome://transparentcrypto/content/graph_sigma.xul",
+		"transparentcrypto-window-graph-sigma",
 		"chrome,width=600,height=400");
 };
