7536778aafc83d7a3951b480382785fc176eeaca
Moritz Schlarb <moschlar@metalabs.de>
2014-07-11 00:25:07 +0200

    Why does it need that alert to work?

--- chrome/content/messengercompose.js
+++ chrome/content/messengercompose.js
@@ -2,8 +2,13 @@
 var transparentcrypto = {};
 
 transparentcrypto.preview = function() {
-    window.open(
+    var editor = window.gMsgCompose.editor;
+    var text = editor.outputToString('text/plain', editor.eNone)
+    //alert(text);
+    var window_preview = window.open(
         "chrome://transparentcrypto/content/preview.xul",
         "transparentcrypto-window-preview",
         "chrome,width=400,height=300");
+    alert(window_preview);
+    window_preview.document.getElementById('encrypted-data').innerHTML = 'CRYPTCRYPTCRYPT' + text + 'CRYPTCRYPTCRYPT';
 };
