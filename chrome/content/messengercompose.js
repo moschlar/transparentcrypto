0ad99746dad00e60e591818293821f5396eb961e
Moritz Schlarb <moschlar@metalabs.de>
2014-07-13 18:52:50 +0200

    Preview window working with plain text code

--- chrome/content/messengercompose.js
+++ chrome/content/messengercompose.js
@@ -1,22 +1,114 @@
 Components.utils.import("resource://transparentcrypto/util.jsm");
 
 /* Create namespace */
-var transparentcrypto = {};
+var transparentcrypto = {
 
-transparentcrypto.preview = function() {
-    var editor = window.gMsgCompose.editor;
-    var text = editor.outputToString('text/plain', editor.eNone)
+    editor: null,
+    preview_window: null,
 
-    var window_preview = window.open(
-        "chrome://transparentcrypto/content/preview.xul",
-        "transparentcrypto-window-preview",
-        "chrome,width=400,height=300");
+    showPreviewWindow: function() {
+        log('showPreviewWindow');
+        log(this.preview_window);
+        try {
+            this.preview_window = window.open(
+                "chrome://transparentcrypto/content/preview.xul",
+                "transparentcrypto-preview-window",
+                "chrome,width=400,height=300");
+            return this.preview_window;
+        } catch (ex) {
+            log(ex);
+            return null;
+        };
+    },
 
-    log('messengercompose.js: ' + window_preview);
+    getEditorContent: function() {
+        log('getEditorContent');
+        try {
+            return this.editor.outputToString('text/plain', this.editor.eNone);
+        } catch (ex) {
+            log(ex);
+            return '';
+        };
+    },
 
-    alert(window_preview);
+    makePreview: function(data) {
+        log('makePreview');
+        return data;
+    },
 
-    window_preview.document.getElementById('encrypted-data').innerHTML = 'CRYPTCRYPTCRYPT' + text + 'CRYPTCRYPTCRYPT';
-};
+    setPreviewContent: function(data) {
+        log('setPreviewContent');
+        try {
+            var elem = this.preview_window.document.getElementById('encrypted-data');
+            elem.innerHTML = data;
+        } catch (ex) { log(ex); };
+    },
 
-log('messengercompose.js: ' + 'loaded')
+    updatePreview: function() {
+        log('updatePreview');
+        if (this.preview_window) {
+            try {
+                var data = this.getEditorContent();
+                data = this.makePreview(data);
+                this.setPreviewContent(data);
+            } catch (ex) { log(ex); };
+        } else {
+            log('No preview window')
+        }
+        log(gMsgCompose.compFields.otherRandomHeaders);
+   },
+
+    myEditorObserver: {
+        /* http://mxr.mozilla.org/comm-central/source/mozilla/editor/idl/nsIEditorObserver.idl#21 */
+        EditAction: function() {
+            log('EditAction');
+            transparentcrypto.updatePreview();
+/*
+            try {
+                log(transparentcrypto.window_preview);
+                log(gMsgCompose.editor);
+                if ((typeof transparentcrypto.window_preview != 'undefined')
+                    && (transparentcrypto.window_preview)) {
+                    transparentcrypto.window_preview.document.getElementById('encrypted-data').innerHTML = gMsgCompose.editor.outputToString('text/plain', gMsgCompose.editor.eNone);
+                }
+            } catch (ex) { log(ex); };
+*/
+        }
+    },
+
+    myStateListener: {
+        /* http://mxr.mozilla.org/comm-central/source/mailnews/compose/public/nsIMsgCompose.idl#73 */
+        NotifyComposeFieldsReady: function() {
+            log('NotifyComposeFieldsReady');
+            try {
+                gMsgCompose.editor.addEditorObserver(transparentcrypto.myEditorObserver);
+            } catch (ex) { log(ex); };
+            try {
+                transparentcrypto.editor = gMsgCompose.editor;
+            } catch (ex) { log(ex); };
+        },
+        NotifyComposeBodyReady: function() {},
+        ComposeProcessDone: function(aResult) {},
+        SaveInFolderDone: function(folderURI) {}
+    },
+
+    composeWindowInit: function() {
+        log('composeWindowInit');
+         try {
+            gMsgCompose.RegisterStateListener(transparentcrypto.myStateListener);
+        } catch (ex) { log(ex); };
+    },
+
+    init: function() {
+        log('init');
+        try {
+            window.addEventListener("compose-window-init", transparentcrypto.composeWindowInit, true);
+        } catch (ex) { log(ex); };
+    },
+}
+
+log('messengercompose.js: ' + 'loaded');
+
+try {
+    transparentcrypto.init();
+} catch (ex) { log(ex); };
