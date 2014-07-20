ab983e07bf0f9b318a858c9ec3b46f3f307214a1
Moritz Schlarb <moschlar@metalabs.de>
2014-07-20 17:07:56 +0200

    Tabs with list and graphs

--- chrome/content/messenger.js
+++ chrome/content/messenger.js
@@ -3,19 +3,12 @@ Components.utils.import("resource://transparentcrypto/util.jsm");
 /* Create namespace */
 var transparentcrypto = {};
 
-transparentcrypto.graph_d3 = function() {
-	log('messenger.js: ' + 'graph_d3');
+transparentcrypto.graph = function() {
+	log('messenger.js: ' + 'graph');
 	window.open(
-		"chrome://transparentcrypto/content/graph_d3.xul",
-		"transparentcrypto-window-graph-d3",
-		"chrome,width=600,height=400");
-};
-transparentcrypto.graph_sigma = function() {
-	log('messenger.js: ' + 'graph_sigma');
-	window.open(
-		"chrome://transparentcrypto/content/graph_sigma.xul",
-		"transparentcrypto-window-graph-sigma",
-		"chrome,width=600,height=400");
+		"chrome://transparentcrypto/content/graph.xul",
+		"transparentcrypto-window-graph",
+		"chrome,width=700,height=400");
 };
 
 log('messenger.js: ' + 'loaded')
