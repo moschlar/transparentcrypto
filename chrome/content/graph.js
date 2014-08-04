7012becfb9b0843cc13ac4e504c003bf08e48ba4
Moritz Schlarb <moschlar@metalabs.de>
2014-08-05 00:56:01 +0200

    WiP keys.jsm

--- chrome/content/graph.js
+++ chrome/content/graph.js
@@ -1,15 +1,8 @@
-Components.utils.import("resource://transparentcrypto/util.jsm");
-
-/* Create namespace */
-var transparentcrypto = {};
-
 try {
+	Components.utils.import("resource://transparentcrypto/util.jsm");
+	Components.utils.import("resource://transparentcrypto/keys.jsm");
 	Components.utils.import("resource://enigmail/enigmailCommon.jsm");
-	Components.utils.import("resource://enigmail/commonFuncs.jsm");
-	Components.utils.import("resource://enigmail/keyManagement.jsm");
-} catch (ex) {Components.utils.reportError(ex);log(ex);}
-
-var keyId = "45D3DBDDFBDD8888";
+} catch (ex) {Components.utils.reportError(ex);}
 
 const keyTrustLevel = {
 	"u": 0,
@@ -45,6 +38,11 @@ const keyTrustName = {
 	}
 };
 
+/* Create namespace */
+var transparentcrypto = {};
+
+var keyId = "45D3DBDDFBDD8888";
+
 var data_d3 = {
 	nodes: [],
 	nodemap: {},
@@ -65,38 +63,12 @@ var data_cy = {
     edges: [],
 };
 
-var keyListObj = {};
-
 try {
-	//EnigmailFuncs.loadKeyList(window, refresh, keyListObj, sortColumn, sortDirection);
-	EnigmailFuncs.loadKeyList(window, false, keyListObj, "userid", 1);
-	log(JSON.stringify(keyListObj));
-} catch (ex) {Components.utils.reportError(ex);log(ex);}
+	var keyList = getKeys(window);
+} catch (ex) {Components.utils.reportError(ex);}
 
-try {
-	//EnigmailFuncs.loadKeyList(window, refresh, keyListObj, sortColumn, sortDirection);
-	EnigmailFuncs.loadKeyList(window, true, keyListObj, "userid", 1);
-	//log(JSON.stringify(keyListObj));
-} catch (ex) {Components.utils.reportError(ex);log(ex);}
-
-/*
-log("keyList")
-for (var i=0; i < keyListObj.keyList.length; ++i) {
-	log(JSON.stringify(keyListObj.keyList[i]));
-}
-*/
-log("keySortList")
-for (var i=0; i < keyListObj.keySortList.length; ++i) {
-	var keySortObj = keyListObj.keySortList[i];
-	log(JSON.stringify(keySortObj));
-	// {"userId":"moritz schlarb <moschlar@metalabs.de>","keyId":"45D3DBDDFBDD8888"}
-	var keyObj = keyListObj.keyList[keySortObj.keyId];
-	log(JSON.stringify(keyObj));
-	// {"expiry":"25.04.2016","expiryTime":1461591907,"created":"27.04.2011","keyId":"45D3DBDDFBDD8888","keyTrust":"u","keyUseFor":"scaESCA","ownerTrust":"u","SubUserIds":[{"userId":"Moritz Schlarb <mail@moritz-schlarb.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb <info@moritz-schlarb.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb <schlarbm@uni-mainz.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb <moschlar@students.uni-mainz.de>","keyTrust":"r","type":"uid"},{"userId":"Moritz Schlarb (TU Darmstadt) <moritz.schlarb@stud.tu-darmstadt.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb (TU Darmstadt) <ak16izaj@rbg.informatik.tu-darmstadt.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb <moritz_schlarb@yahoo.de>","keyTrust":"r","type":"uid"},{"userId":"User attribute (JPEG image)","keyTrust":"r","type":"uat","uatNum":0},{"userId":"User attribute (JPEG image)","keyTrust":"u","type":"uat","uatNum":1},{"userId":"Moritz Schlarb <moschlar@monoceres.uberspace.de>","keyTrust":"u","type":"uid"}],"fpr":"19F9B5A8821A081EFE92D30645D3DBDDFBDD8888","photoAvailable":true,"secretAvailable":true,"userId":"Moritz Schlarb <moschlar@metalabs.de>"}
-	for (var j=0; j < keyObj.SubUserIds.length; ++j) {
-		log(JSON.stringify(keyObj.SubUserIds[j]));
-		// {"userId":"Moritz Schlarb (TU Darmstadt) <moritz.schlarb@stud.tu-darmstadt.de>","keyTrust":"u","type":"uid"}
-	}
+for (var i=0; i < keyList.length; ++i) {
+	var keyObj = keyList[i];
 	try {
 		var userId = EnigConvertGpgToUnicode(keyObj.userId);
 		data_d3.nodemap[keyObj.keyId] = i;
@@ -133,120 +105,55 @@ for (var i=0; i < keyListObj.keySortList.length; ++i) {
 var t = data_d3.nodemap[keyId];
 log(t);
 
-// From enigmailCommon.js
+var sigList = getSigs(keyId);
 
-// see GnuPG doc/DETAILS
-// Format of colon listings
-const keyRowField = {
-	type: 0,
-	keyValidity: 1,
-	keyAlgo: 3, // 1 = RSA, 16 = Elgamal (encryption only), 17 = DSA, 20 = Elgamal (sign and encrypt)
-	keyId: 4,
-	created: 5,
-	expiry: 6,
-	uidId: 7,
-	ownerTrust: 8,
-	userId: 9,
-	sigType: 10, // Signature class as per RFC-4880: x = exportable, l = local-only
-	keyCapabilities: 11, // Key capabilities: e, s, c, a, E, S, C, A, D
-	sigHash: 15, // Used hash algorithm for sig records: 2 = SHA-1, 8 = SHA-256
-};
-
-const keyValidityValue = {
-	invalid: "i",
-	disabled: "d",
-	revoked: "r",
-	expired: "e",
-	notValid: "erid",
-}
-
-// From enigmailViewKeySigDlg.xul
-
-var enigmailSvc = GetEnigmailSvc();
-if (!enigmailSvc) {
-	log(EnigGetString("accessError"));
-}
-
-var exitCodeObj = {};
-var errorMsgObj = {};
-
-var sigList = enigmailSvc.getKeySig("0x"+keyId, exitCodeObj, errorMsgObj);
-
-if (exitCodeObj.value != 0) {
-	log(errorMsgObj.value);
-}
-
-//log(sigList);
-var aSigList = sigList.split(/\n/);
-//log(aSigList);
-
-for (var i=0; i<aSigList.length; ++i) {
-	if (aSigList[i]) {
-		try {
-			var listRow = aSigList[i].split(/:/);
-			log(listRow);
-			log(listRow.length);
-			// sig,,,1,45D3DBDDFBDD8888,1303911907,,,,Moritz Schlarb <moschlar@metalabs.de>,18x,,,,,2,
-			var entry = {
-				type: listRow[keyRowField.type],
-				keyValidity: listRow[keyRowField.keyValidity],
-				keyId: listRow[keyRowField.keyId],
-				created: listRow[keyRowField.created],
-				expiry: listRow[keyRowField.expiry],
-				uidId: listRow[keyRowField.uidId],
-				ownerTrust: listRow[keyRowField.ownerTrust],
-				userId: EnigConvertGpgToUnicode(listRow[keyRowField.userId]),
-				sigType: listRow[keyRowField.sigType],
-				keyUseFor: listRow[keyRowField.keyUseFor],
-			};
-			log(JSON.stringify(entry));
-			//{"type":"sig","keyValidity":"","keyId":"45D3DBDDFBDD8888","created":"1303911907","expiry":"","uidId":"","ownerTrust":"","userId":"Moritz Schlarb <moschlar@metalabs.de>","sigType":"18x"}
-			if (entry.type === "sig") {
-				var s = data_d3.nodemap[entry.keyId];
-				if (!s) {
-					log("###" + entry.keyId);
-					s = data_d3.nodes.length;
-					data_d3.nodemap[entry.keyId] = s;
-					data_d3.nodes.push({
-						group: 5,
-						name: EnigConvertGpgToUnicode(entry.userId),
-					});
-				}
-				data_d3.links.push({
-					target: t,
-					source: s,
+for (var i=0; i < sigList.length; ++i) {
+	var entry = sigList[i];
+	try {
+		//log(JSON.stringify(entry));
+		if (entry.type === "sig") {
+			var s = data_d3.nodemap[entry.keyId];
+			if (!s) {
+				log("###" + entry.keyId);
+				s = data_d3.nodes.length;
+				data_d3.nodemap[entry.keyId] = s;
+				data_d3.nodes.push({
+					group: 5,
+					name: EnigConvertGpgToUnicode(entry.userId),
 				});
+			}
+			data_d3.links.push({
+				target: t,
+				source: s,
+			});
 
-				if (!data_sigma.nodemap[entry.keyId]) {
-					log("###" + entry.keyId);
-					data_sigma.nodemap[entry.keyId] = true;
-					data_sigma.nodes.push({
-						id: entry.keyId,
-					});
-				}
-				var name = keyId + '-' + entry.keyId;
-				if (!data_sigma.edgemap[name]) {
-					log("###" + entry.keyId);
-					data_sigma.edgemap[name] = true;
-					data_sigma.edges.push({
-						id: name,
-						target: keyId,
-						source: entry.keyId,
-					});
-				}
-				data_vis.edges.push({
-					to: keyId,
-					from: entry.keyId,
-					//label: "",
-					title: entry.created,
+			if (!data_sigma.nodemap[entry.keyId]) {
+				log("###" + entry.keyId);
+				data_sigma.nodemap[entry.keyId] = true;
+				data_sigma.nodes.push({
+					id: entry.keyId,
 				});
-				data_cy.edges.push({data: {
+			}
+			var name = keyId + '-' + entry.keyId;
+			if (!data_sigma.edgemap[name]) {
+				log("###" + entry.keyId);
+				data_sigma.edgemap[name] = true;
+				data_sigma.edges.push({
+					id: name,
 					target: keyId,
 					source: entry.keyId,
-				}});
+				});
 			}
-		} catch (ex) {Components.utils.reportError(ex);log(ex);}
-	}
+			data_vis.edges.push({
+				to: keyId,
+				from: entry.keyId,
+			});
+			data_cy.edges.push({data: {
+				target: keyId,
+				source: entry.keyId,
+			}});
+		}
+	} catch (ex) {Components.utils.reportError(ex);log(ex);}
 }
 
 //log("graph.js: " + JSON.stringify(data_vis));
---
+++ modules/keys.jsm
@@ -0,0 +1,149 @@
+var EXPORTED_SYMBOLS = [ "getKeys", "getSigs" ];
+
+const Cc = Components.classes, Ci = Components.interfaces, Cu = Components.utils;
+
+try {
+	Cu.import("resource://transparentcrypto/util.jsm");
+	Cu.import("resource://enigmail/enigmailCommon.jsm");
+	Cu.import("resource://enigmail/commonFuncs.jsm");
+	Cu.import("resource://enigmail/keyManagement.jsm");
+} catch (ex) {Cu.reportError(ex);}
+
+// see GnuPG doc/DETAILS
+// Format of colon listings
+const sigRowField = {
+	type: 0,
+	keyValidity: 1,
+	keyAlgo: 3, // 1 = RSA, 16 = Elgamal (encryption only), 17 = DSA, 20 = Elgamal (sign and encrypt)
+	keyId: 4,
+	created: 5,
+	expiry: 6,
+	uidId: 7,
+	ownerTrust: 8,
+	userId: 9,
+	sigType: 10, // Signature class as per RFC-4880: x = exportable, l = local-only
+	keyCapabilities: 11, // Key capabilities: e, s, c, a, E, S, C, A, D
+	sigHash: 15, // Used hash algorithm for sig records: 2 = SHA-1, 8 = SHA-256
+};
+
+function getKeys(window) {
+
+	var keys = [];
+
+	var keyListObj = {};
+
+	try {
+		//EnigmailFuncs.loadKeyList(window, refresh, keyListObj, sortColumn, sortDirection);
+		EnigmailFuncs.loadKeyList(window, false, keyListObj, "userid", 1);
+	} catch (ex) {
+		Components.utils.reportError(ex);
+		log(ex);
+		return undefined;
+	}
+
+	for (var i=0; i < keyListObj.keySortList.length; ++i) {
+		var keySortObj = keyListObj.keySortList[i];
+		var keyObj = keyListObj.keyList[keySortObj.keyId];
+		keys.push(keyObj);
+	}
+
+	return keys;
+
+/*
+	for (var i=0; i < keyListObj.keySortList.length; ++i) {
+		var keySortObj = keyListObj.keySortList[i];
+		log(JSON.stringify(keySortObj));
+		// {"userId":"moritz schlarb <moschlar@metalabs.de>","keyId":"45D3DBDDFBDD8888"}
+		var keyObj = keyListObj.keyList[keySortObj.keyId];
+		log(JSON.stringify(keyObj));
+		// {"expiry":"25.04.2016","expiryTime":1461591907,"created":"27.04.2011","keyId":"45D3DBDDFBDD8888","keyTrust":"u","keyUseFor":"scaESCA","ownerTrust":"u","SubUserIds":[{"userId":"Moritz Schlarb <mail@moritz-schlarb.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb <info@moritz-schlarb.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb <schlarbm@uni-mainz.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb <moschlar@students.uni-mainz.de>","keyTrust":"r","type":"uid"},{"userId":"Moritz Schlarb (TU Darmstadt) <moritz.schlarb@stud.tu-darmstadt.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb (TU Darmstadt) <ak16izaj@rbg.informatik.tu-darmstadt.de>","keyTrust":"u","type":"uid"},{"userId":"Moritz Schlarb <moritz_schlarb@yahoo.de>","keyTrust":"r","type":"uid"},{"userId":"User attribute (JPEG image)","keyTrust":"r","type":"uat","uatNum":0},{"userId":"User attribute (JPEG image)","keyTrust":"u","type":"uat","uatNum":1},{"userId":"Moritz Schlarb <moschlar@monoceres.uberspace.de>","keyTrust":"u","type":"uid"}],"fpr":"19F9B5A8821A081EFE92D30645D3DBDDFBDD8888","photoAvailable":true,"secretAvailable":true,"userId":"Moritz Schlarb <moschlar@metalabs.de>"}
+		for (var j=0; j < keyObj.SubUserIds.length; ++j) {
+			log(JSON.stringify(keyObj.SubUserIds[j]));
+			// {"userId":"Moritz Schlarb (TU Darmstadt) <moritz.schlarb@stud.tu-darmstadt.de>","keyTrust":"u","type":"uid"}
+		}
+		try {
+			var userId = EnigConvertGpgToUnicode(keyObj.userId);
+			data_d3.nodemap[keyObj.keyId] = i;
+			data_d3.nodes.push({
+				group: keyTrustLevel.get(keyObj.keyTrust) + 1,
+				name: userId,
+			});
+			data_sigma.nodemap[keyObj.keyId] = true;
+			data_sigma.nodes.push({
+				id: keyObj.keyId,
+			});
+			data_vis.nodes.push({
+				id: keyObj.keyId,
+				level: keyTrustLevel.get(keyObj.keyTrust),
+				group: keyObj.keyTrust,
+				label: userId.replace(" <", "\n<"),
+				title: "<b>userId:</b> " + escapeHTML(userId) + "<br />" +
+					"<b>keyId:</b> " + keyObj.keyId + "<br />" +
+					"<b>keyTrust:</b> " + keyObj.keyTrust + " (" + keyTrustName.get(keyObj.keyTrust) + ")" + "<br />" +
+					"<b>ownerTrust:</b> " + keyObj.ownerTrust + "<br />" +
+					"<b>secretAvailable:</b> " + keyObj.secretAvailable + "<br />" +
+					"<b>created:</b> " + keyObj.created + "<br />" +
+					"<b>expiry:</b> " + keyObj.expiry + "<br />" +
+					"",
+			});
+			data_cy.nodes.push({data: {
+				id: keyObj.keyId,
+				name: userId,
+			}});
+		} catch (ex) {Components.utils.reportError(ex);log(ex);}
+	}
+*/
+}
+
+function getSigs(keyId) {
+
+	var sigs = [];
+
+	// From enigmailViewKeySigDlg.xul
+
+	var enigmailSvc = GetEnigmailSvc();
+	if (!enigmailSvc) {
+		log(EnigGetString("accessError"));
+		return undefined;
+	}
+
+	var exitCodeObj = {};
+	var errorMsgObj = {};
+
+	var sigList = enigmailSvc.getKeySig("0x" + keyId, exitCodeObj, errorMsgObj);
+
+	if (exitCodeObj.value != 0) {
+		log(errorMsgObj.value);
+		return undefined;
+	}
+
+	//log(sigList);
+	var aSigList = sigList.split(/\n/);
+	//log(aSigList);
+
+	for (var i=0; i<aSigList.length; ++i) {
+		if (aSigList[i]) {
+			var listRow = aSigList[i].split(/:/);
+			//log(listRow);
+			//log(listRow.length);
+			// sig,,,1,45D3DBDDFBDD8888,1303911907,,,,Moritz Schlarb <moschlar@metalabs.de>,18x,,,,,2,
+			var entry = {
+				type: listRow[keyRowField.type],
+				keyValidity: listRow[keyRowField.keyValidity],
+				keyId: listRow[keyRowField.keyId],
+				created: listRow[keyRowField.created],
+				expiry: listRow[keyRowField.expiry],
+				uidId: listRow[keyRowField.uidId],
+				ownerTrust: listRow[keyRowField.ownerTrust],
+				userId: EnigConvertGpgToUnicode(listRow[keyRowField.userId]),
+				sigType: listRow[keyRowField.sigType],
+				keyUseFor: listRow[keyRowField.keyUseFor],
+			};
+			sigs.push(entry);
+		}
+	}
+
+	return sigs;
+}
+
+log('keys.jsm: ' + 'loaded')
